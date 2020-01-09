b6f8de946e3803647497d638e9de13899af2ea03
Liam Tan <liam.tan@uq.edu.au>
2020-01-09 14:54:28 +1000

    Make UsersList independent

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { IStore, IUser } from "../types";
+import { fetchUsers } from "../actions/api";
 
 export default function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   const users: IUser[] = useSelector((state: IStore) => state.apiState.users);
   return (
     <ul>
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,16 +1,7 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-
+import React from "react";
 import UsersList from "../components/UsersList";
-import { fetchUsers } from "../actions/api";
 
 export default function AvailabilityPage() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, []);
-
   return (
     <>
       <h1>Users page</h1>
